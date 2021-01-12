103ece3143be07deebcc8277825ed97c7cead053
spaceC00kie <kirstie317@gmail.com>
2021-01-12 00:53:14 -0500

    array times two in python and js

--- practice-12.js
+++ practice-12.js
@@ -5,3 +5,6 @@ arr = ["bob", "linda", "tina", "gene", "louise"]
 const stringLength = (arr) => arr.map(i => i.length)
 
 console.log(stringLength(arr))
+
+
+//write a function that accepts an integer array, doubles each item, then returns mutated array
---
+++ practice-13.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3, 4, 5]
+
+const timesTwo = (arr) => arr.map((i) => i * 2)
+
+console.log(timesTwo(arr))
